c9dedd8810457a394ca3e58ed3ddd31bee3e28ea
hitzu <hitzu1001@gmail.com>
2020-02-01 21:57:15 +1100

    add PhotoDetailForm

---
+++ src/components/PhotoDetailForm.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput, Alert } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
